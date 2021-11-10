d069dd7fc8227ea6e69649925401d6d552f421ec
gaianacheli <gaianacheli04@gmail.com>
2021-11-10 15:44:33 +0100

    componente title

---
+++ vueclifirst/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Title msg="La mia prima app con Vue CLI!" />
+  </div>
+</template>
+
+<script>
+import Title from './components/Title.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Title
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
