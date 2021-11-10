b3a52e5eadc495a21074d0d3de7b1c66bceecbe2
gaianacheli <gaianacheli04@gmail.com>
2021-11-10 15:56:42 +0100

    bonus: componente hero aggiunto

--- vueclifirst/src/components/hero.vue
+++ vueclifirst/src/components/hero.vue
@@ -1,6 +1,18 @@
 <template>
 <div class="jumbotron">
-    
+    <h2>Jumbotron</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing Lorem ipsum 
+        dolor sit amet consectetur adipisicing elit. Fugiat, 
+        assumenda. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Eaque, placeat.
+    </p>
+    <button class="btn">Scopri</button>
 </div>
     
 </template>
+
+<script>
+export default {
+    
+};
+</script>
